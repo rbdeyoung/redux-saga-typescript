ecb9eda45d739da17dc2960299dc438117c37024
Rich DeYoung <richard.deyoung@gmail.com>
2018-03-12 22:34:45 -0500

    Added Typescript support

---
+++ src/TestComponent.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class TestComponent extends React.Component<{}, {}> {
+    public render() {
+        return (
+            <div className="testComponent">
+                This is a test component
+            </div>
+        );
+    }
+}
+
+export default TestComponent;
