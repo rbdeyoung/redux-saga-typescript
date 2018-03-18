849c8bce48c5ecd95d51e91be943722638e1ec3d
Rich DeYoung <12941684+rbdeyoung@users.noreply.github.com>
2018-03-18 18:36:36 -0500

    Added some material-ui structure

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react'
-import {TestComponent} from './TestComponent'
-import {RootState} from '../app/reducers/index'
+import {RootState} from '../app/reducers'
 import {updateCounter} from './actions/creators'
 import {CounterOperation} from './types'
 import {connect, Dispatch} from 'react-redux'
 import {withStyles} from 'material-ui/styles'
 import Paper from 'material-ui/Paper'
 import Tabs, {Tab} from 'material-ui/Tabs'
+import createMuiTheme from 'material-ui/styles/createMuiTheme'
+import './Home.css'
+import Grid from 'material-ui/Grid'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
 
 // import {MuiThemeProvider, createMuiTheme} from 'material-ui/styles'
+// const theme = createMuiTheme({})
 
 export interface HomePageStateProps {
     counter: number
@@ -30,6 +36,14 @@ const decorate = withStyles(({palette, spacing}) => ({
         backgroundColor: palette.background,
         color: palette.primary,
     },
+    paper: {
+        padding: spacing.unit * 2,
+        textAlign: 'center',
+        color: palette.text.secondary,
+    },
+    menuButton: {
+
+    }
 }))
 
 const onChange = () => undefined
@@ -40,26 +54,41 @@ const onChange = () => undefined
 
 const DecoratedHomePageComponent = decorate<HomeProps>(({counter, onIncrement, onDecrement, classes}) => (
 
-    <div>
-        <Paper className={classes.root}>
-            <Tabs
-                value={5}
-                onChange={onChange}
-                indicatorColor="primary"
-                textColor="primary"
-                centered={true}
-            >
-                <Tab label="Simple Redux Demo"/>
-                <Tab label="Saga Demo"/>
-            </Tabs>
-        </Paper>
-        <div>
-            <div>
-                <button onClick={onDecrement}>-</button>
-                {counter}
-                <button onClick={onIncrement}>+</button>
-            </div>
-            <TestComponent/>
+    <div className={classes.root}>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="title" color="inherit">
+                    Title
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        <div className="Home">
+            <Grid container={true} spacing={24}>
+                <Grid item={true} xs={12}>
+                    <Paper>
+                        <Tabs
+                            value={5}
+                            onChange={onChange}
+                            indicatorColor="primary"
+                            textColor="primary"
+                            centered={true}
+                        >
+                            <Tab label="Simple Redux Demo"/>
+                            <Tab label="Saga Demo"/>
+                        </Tabs>
+
+                    </Paper>
+                </Grid>
+                <Grid item={true} xs={8} sm={4}>
+                    <button onClick={onDecrement}>Decrement</button>
+                </Grid>
+                <Grid item={true} xs={8} sm={4}>
+                    <p className="counter">{counter}</p>
+                </Grid>
+                <Grid item={true} xs={8} sm={4}>
+                    <button onClick={onIncrement}>Increment</button>
+                </Grid>
+            </Grid>
         </div>
     </div>
 ))
