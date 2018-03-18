81c36a3dbdae274c5a5cd2bcc306d247b00349f0
Rich DeYoung <12941684+rbdeyoung@users.noreply.github.com>
2018-03-18 16:36:50 -0500

    Server/Client MUI rendering now works (I think)

--- src/client.tsx
+++ src/client.tsx
@@ -1,13 +1,42 @@
 import React from 'react'
-import { hydrate } from 'react-dom'
-import { Root } from './app/Root'
-import { configureStore } from './app/store/configureStore'
+import {hydrate} from 'react-dom'
+import {Root} from './app/Root'
+import {configureStore} from './app/store/configureStore'
 import createHistory from 'history/createHashHistory'
+import {Store} from 'redux'
+import createMuiTheme from 'material-ui/styles/createMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 const store = configureStore(createHistory(), window.__PRELOADED_STATE__)
 
-hydrate(<Root store={store} />, document.getElementById('root'))
+interface Props {
+    store: Store<RootState>
+}
+class MuiWrapperComponent extends React.Component<Props> {
+
+    constructor(props: Props) {
+        super(props)
+    }
+
+    public componentDidMount() {
+        const jssStyles = document.getElementById('jss-server-side')
+        if (jssStyles && jssStyles.parentNode) {
+            jssStyles.parentNode.removeChild(jssStyles)
+        }
+    }
+
+    public render() {
+        return <Root {...this.props} />
+    }
+}
+
+const theme = createMuiTheme({})
+
+hydrate(
+    <MuiThemeProvider theme={theme}>
+        <MuiWrapperComponent store={store}/>
+    </MuiThemeProvider>, document.getElementById('root'))
 
 if (module.hot) {
-  module.hot.accept()
+    module.hot.accept()
 }
